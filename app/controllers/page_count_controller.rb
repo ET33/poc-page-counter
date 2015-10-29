6ecac181aa8b85d258288834e043cc44b666123b
Edylson T <tedylson@outlook.com>
2015-10-29 15:03:02 -0200

    Counts number of visit when news/show is accessed

--- app/controllers/page_count_controller.rb
+++ app/controllers/page_count_controller.rb
@@ -1,6 +1,9 @@
 class PageCountController < ApplicationController
   def inc_page_count
+    $redis.zincrby(:page_counter, 1, params[:uri])
     render nothing: true
-    # TODO gonna use redis here?
   end
+
+  # To get top 10 readed: `$redis.zrangebyscore(:page_counter, :'-inf', :'+inf').reverse`
+  # optional: `withscore: true`
 end
